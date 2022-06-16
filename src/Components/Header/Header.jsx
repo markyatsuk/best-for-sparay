949e960bdccc0064ffea44bed17321935e34caaf
Mark <markyatsuk12.04.16@gmail.com>
2022-06-17 00:41:13 +0200

    fix logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import "./Header.scss";
+import logo from "./logo.png";
 function RenderHeader() {
   return (
     <header>
       <a href="ds">
-        <img src="./logo.png" alt="" width={50} />
+        <img src={logo} alt="" />
       </a>
       <form action="submit">
         <input type="text" />
